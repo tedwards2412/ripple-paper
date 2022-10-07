ef000184ee49fb99e9f3cd12f90bb93d04c58706
Thomas Edwards <tedwards2412@gmail.com>
2022-10-07 16:37:14 -0400

    Wrote more of section 2

--- src/scripts/random_matches.py
+++ src/scripts/random_matches.py
@@ -36,21 +36,27 @@ data = np.loadtxt(paths.data / "ripple_phenomD_matches.txt")
 exactmatch_indices = (1.0 - data[:, -1]) <= 0.0
 other_indices = (1.0 - data[:, -1]) > 0.0
 
+q = data[:, 1] / data[:, 0]
+chieff = (data[:, 0] * data[:, 2] + data[:, 1] * data[:, 3]) / (data[:, 0] + data[:, 1])
+Mtot = data[:, 1] + data[:, 0]
+
 # Plot and save
 plt.figure(figsize=(7, 5))
 cm = plt.cm.get_cmap("inferno")
 sc = plt.scatter(
-    data[other_indices, 0],
-    data[other_indices, 1],
+    q[other_indices],
+    chieff[other_indices],
     c=np.log10(1.0 - data[other_indices, -1]),
     cmap=cm,
 )
 plt.colorbar(sc, label=r"$\log_{10}(1-\mathrm{Match})$")
 
-plt.scatter(data[exactmatch_indices, 0], data[exactmatch_indices, 1], color="C0")
-plt.xlabel(r"$m_1 \,\left[M_{\odot}\right]$")
-plt.ylabel(r"$m_2 \,\left[M_{\odot}\right]$")
-plt.xlim(1, 50)
-plt.ylim(1, 50)
+plt.scatter(q[exactmatch_indices], chieff[exactmatch_indices], color="C0")
+# plt.xlabel(r"Total Mass, $M$")
+# plt.ylabel(r"Mass Ratio, $q$")
+plt.xlabel(r"Mass Ratio, $q$")
+plt.ylabel(r"Effective Spin, $\chi_{\rm eff}$")
+# plt.xlim(1, 50)
+# plt.ylim(1, 50)
 
 plt.savefig(paths.figures / "random_matches.pdf", bbox_inches="tight")
