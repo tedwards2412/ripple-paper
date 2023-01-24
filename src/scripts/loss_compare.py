829b27b14ddc183d45873632b435b4aafc9a12aa
tedwards2412 <tedwards2412@gmail.com>
2023-01-24 17:39:54 -0500

    bunch of updates

--- src/scripts/loss_compare.py
+++ src/scripts/loss_compare.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import paths
+# from ripple.waveforms import IMRPhenomD_utils
 
 params = {
     "font.size": 18,
@@ -39,13 +40,29 @@ mpl.rcParams.update(params)
 
 
 plt.axvline(x=0.018, color="k", alpha=0.5, ls="--")
-# fRD =
-# 0.5fRD
-plt.axvline(x=0.08, color="k", alpha=0.4, ls="--")
+
+# theta = np.array([25.0, 25.0, -0.8, -0.8])
+
+# # Calculate the frequency regions
+# coeffs = IMRPhenomD_utils.get_coeffs(theta)
+# f1, f2, _, _, _, _ = IMRPhenomD_utils.get_transition_frequencies(
+#     theta, coeffs[5], coeffs[6]
+# )
+
+# print(f2)
+MSUN = 1.988409902147041637325262574352366540e30  # kg
+G = 6.67430e-11  # m^3 / kg / s^2
+C = 299792458.0  # m / s
+gt = G * MSUN / (C ** 3.0)
+
+f2 =  149.1018563859192 * (25. + 25.) * gt
+plt.axvline(x=f2, color="k", alpha=0.4, ls="--")
+
 plt.plot(freq, ori_error, label="Original")
 plt.plot(freq, opt_error, label="Optimized")
 plt.legend()
 plt.xlabel(r"$Mf$")
 plt.ylabel(r"$\Delta|\tilde{h}(f)|$")
-plt.xlim(0.01, 0.1)
+plt.xlim(0.01, 0.09)
+plt.ylim(0.0, 0.05)
 plt.savefig(paths.figures / "loss_compare.pdf", bbox_inches="tight")
