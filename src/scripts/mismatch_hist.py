0cf00d4e147c9fffa1f2e72f0e3297525ab98d7e
tedwards2412 <tedwards2412@gmail.com>
2023-01-26 12:25:51 -0500

    Getting ready for P&P

--- src/scripts/mismatch_hist.py
+++ src/scripts/mismatch_hist.py
@@ -39,25 +39,23 @@ mpl.rcParams.update(params)
 (catalog_num, ori_loss, opt_loss) = np.loadtxt(
     paths.data / "mismatch_hist.txt", unpack=True
 )
+
+print("Median Change is:", (np.median(opt_loss) - np.median(ori_loss))*100/np.median(ori_loss))
+
 bins = np.arange(-5, -1, 0.1)
-
-print(np.mean(ori_loss))
-print(np.mean(opt_loss))
-# print((np.mean(ori_loss) - np.mean(opt_loss))*100/np.mean(ori_loss))
-
 # print(stats.mode(np.log10(ori_loss)[:]))
 # print(np.mean(np.log10(ori_loss)) / np.mean(np.log10(opt_loss)))
 # print(stats.mode(np.log10(ori_loss))[0] / stats.mode(np.log10(opt_loss))[0])
 
-plt.axvline(x=np.mean(np.log10(ori_loss)), color="C0", ls="--")
+plt.axvline(x=np.log10(np.median(ori_loss)), color="C0", ls="--")
 plt.hist(np.log10(ori_loss), bins=bins, alpha=0.3, color="C0")
 plt.hist(np.log10(ori_loss), bins=bins, label="Original", histtype="step", color="C0")
 
-plt.axvline(x=np.mean(np.log10(opt_loss)), color="C1", ls="--")
+plt.axvline(x=np.log10(np.median(opt_loss)), color="C1", ls="--")
 plt.hist(np.log10(opt_loss), bins=bins, alpha=0.3, color="C1")
 plt.hist(np.log10(opt_loss), bins=bins, label="Optimized", histtype="step", color="C1")
 plt.legend()
 plt.xlim(-5, -1)
-plt.xlabel("$\log(\mathcal{M})$")
+plt.xlabel("$\log_{10}(\mathcal{M})$")
 plt.ylabel("Count")
 plt.savefig(paths.figures / "mismatch_hist.pdf", bbox_inches="tight")
